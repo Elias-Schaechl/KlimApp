5d60a13476cf5f743990886363a0abcddcf7b900
Elias-Schaechl <32268758+Elias-Schaechl@users.noreply.github.com>
2023-01-07 22:07:43 +0100

    angular integration

---
+++ angular-klim-app/src/app/components/community-goal/community-goal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-community-goal',
+  templateUrl: './community-goal.component.html',
+  styleUrls: ['./community-goal.component.css']
+})
+export class CommunityGoalComponent {
+
+}
